ce57e3f4e68d082d6560246043d4852d80b4c157
Natoteo <natotealukas@gmail.com>
2018-02-09 13:08:45 +0100

    Commit

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -10,3 +10,42 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+#include "sorting_algorithms.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void init_random(int* small_array, int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+      small_array[i] = rand();
+  }
+}
+void bubble_sort(int* small_array, int size)
+{
+  int temp = 0;
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      if(small_array[i] < small_array[j])
+      {
+        temp = small_array[i];
+        small_array[i] = small_array[j];
+        small_array[j] = small_array[i];
+      }
+    }
+  }
+}
+void insertion_sort(int* small_array, int size)
+{
+  for (int i = 1; i < size; i++)
+   {
+       int number = small_array[i];
+       int j = i-1;
+       while (j >= 0 && small_array[j] > number)
+       {
+           small_array[j+1] = small_array[j];
+           j = j-1;
+       }
+       small_array[j+1] = number;
+   }
+}
--- sorting_algorithms.h
+++ sorting_algorithms.h
@@ -20,6 +20,8 @@
 *** Basically two functions to sort arrays of integer elements. Furthermore, a function
 *** to initialize an array with a number of random elements.
 */
-
+void init_random(int* small_array, int size);
+void bubble_sort(int* small_array, int size);
+void insertion_sort(int* small_array, int size);
 
 #endif
